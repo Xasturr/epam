0273674ff8d744c41d25ab6098423aba8212f3b3
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-04-25 12:31:38 +0300

    .

---
+++ lab5/script.js
@@ -0,0 +1,46 @@
+let url = 'https://randomuser.me/api/?results=50';
+let picture = document.getElementsByClassName("picture")[0];
+
+function getHttp(url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+
+        xhr.onload = function () {
+            if (this.status == 200) {
+                resolve(JSON.parse(this.response));
+            } else {
+                let err = new Error(this.statusText);
+                err.code = this.status;
+                reject(err);
+            }
+        }
+
+        xhr.onerror = function () {
+            reject(new Error("Network error"));
+        };
+
+        xhr.send();
+    });
+}
+
+getHttp(url)
+    .then(res => {
+        res.results.forEach(element => {
+            let img = document.createElement('img');
+            img.src = element.picture.large;
+            img.class = 'img';
+            img.alt = 'image';
+            img.style = 'margin: auto; margin-bottom: 80px';
+            picture.appendChild(img);
+            // console.log(element.picture.large);
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+function OnMouseMove(event) {
+    let posX = event.clientX, posY = event.clientY;
+    console.log(posX, posY);
+}
