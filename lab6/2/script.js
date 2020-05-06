5c409da91ff37681cbaa2e95ea4d06712d5c22d4
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-05-06 21:29:00 +0300

    .

--- lab6/2/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {});
-
-$.get("http://dummy.restapiexample.com/api/v1/employees", function (data, status) {
-    data = JSON.stringify(data); 
-    JSON.parse(data).data.forEach(element => {
-        $('.employee').append('<div>Name: ' + element.employee_name + '; Salary: ' +
-        element.employee_salary + '; Age: ' + element.employee_age + '</div>');
-    });
-});
