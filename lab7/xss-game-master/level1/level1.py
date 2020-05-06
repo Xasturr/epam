31658eda7d1a7cbde675eb59c23e0e3963323361
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-05-06 23:25:28 +0300

    level1 fix

--- lab7/xss-game-master/level1/level1.py
+++ lab7/xss-game-master/level1/level1.py
@@ -1,4 +1,3 @@
-import webapp2 as webapp
 page_header = """
 <!doctype html>
 <html>
@@ -12,13 +11,13 @@ page_header = """
     <img src="https://xss-game.appspot.com/static/logos/level1.png">
       <div>
 """
-
+ 
 page_footer = """
     </div>
   </body>
 </html>
 """
-
+ 
 main_page_markup = """
 <form action="" method="GET">
   <input id="query" name="query" value="Enter query here..."
@@ -26,40 +25,37 @@ main_page_markup = """
   <input id="button" type="submit" value="Search">
 </form>
 """
-
-
+import webapp2 as webapp
+import cgi
 class MainPage(webapp.RequestHandler):
-
-    def render_string(self, s):
-        self.response.out.write(s)
-
-    def get(self):
-        # Disable the reflected XSS filter for demonstration purposes
-        self.response.headers.add_header("X-XSS-Protection", "0")
-
-        if not self.request.get('query'):
-            # Show main search page
-            self.render_string(page_header + main_page_markup + page_footer)
-        else:
-            query = self.request.get('query', '[empty]')
-
-            # Our search engine broke, we found no results :-(
-            message = "Sorry, no results were found for <b>" + query + "</b>."
-            message += " <a href='?'>Try again</a>."
-
-            # Display the results page
-            self.render_string(page_header + message + page_footer)
-
-        return
-
-
-application = webapp.WSGIApplication([('.*', MainPage), ], debug=False)
-
+ 
+  def render_string(self, s):
+    self.response.out.write(s)
+ 
+  def get(self):
+    # Disable the reflected XSS filter for demonstration purposes
+    self.response.headers.add_header("X-XSS-Protection", "0")
+ 
+    if not self.request.get('query'):
+      # Show main search page
+      self.render_string(page_header + main_page_markup + page_footer)
+    else:
+      query = self.request.get('query', '[empty]')
+       
+      # Our search engine broke, we found no results :-(
+      message = "Sorry, no results were found for <b>" + cgi.escape(query) + "</b>."
+      message += " <a href='?'>Try again</a>."
+ 
+      # Display the results page
+      self.render_string(page_header + message + page_footer)
+     
+    return
+ 
+application = webapp.WSGIApplication([ ('.*', MainPage), ], debug=False)
 
 def main():
     from paste import httpserver
     httpserver.serve(application, host='127.0.0.1', port='8080')
-
 
 if __name__ == '__main__':
     main()
