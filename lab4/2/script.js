03030f49f60779b90d980fe2be6eb7da305d7d2b
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-05-07 08:11:12 +0300

    lab4

--- lab4/2/script.js
+++ lab4/2/script.js
@@ -11,12 +11,7 @@ let tempNameInput;
 let tempPriceInput;
 let mainDiagram = document.getElementsByClassName("mainDiagram")[0];
 
-// console.log(mainDiagram);
-
-// console.log(table.rows.length);
-
 for (let i = 1; i < table.rows.length; i++) {
-    console.log(table.rows[i]);
     let newDiv = document.createElement("div");
 
     let newContentName = document.createTextNode(table.rows[i].cells[1].dataset.name); 
@@ -69,23 +64,16 @@ function saveBtnClick() {
         else 
             name = this.parentNode.childNodes[1].dataset.name;
 
-        console.log("NAME");
-        console.log(table.rows[1].cells[1].dataset.name);
-        console.log("NAME");
-
         for (let i = 1; i < table.rows.length; i++) {
             if (table.rows[i].cells[1].dataset.name == tempNameInput &&
                 table.rows[i].cells[2].dataset.price == tempPriceInput) {
                 table.rows[i].remove();
-                //console.log(tempNameInput);
                 break;
             }
         }
         for (let i = 1; i < mainDiagram.childNodes.length; i++) {
             if (mainDiagram.childNodes[i].innerHTML == tempNameInput) {
-                //console.log(mainDiagram.childNodes[i].innerHTML);
                 mainDiagram.childNodes[i].remove();
-                //console.log(tempNameInput);
                 break;
             }
         }
@@ -95,9 +83,6 @@ function saveBtnClick() {
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
-
-    console.log("Name input:");
-    console.log(nameInput.value);
 
     cell1.innerHTML = "видалити";
     cell1.className = "deleteRow";
@@ -112,7 +97,6 @@ function saveBtnClick() {
     cell3.setAttribute("class", "changeInfo");
     cell3.setAttribute("onclick", "changeBtnClick(this)");
     cell3.innerHTML = priceInput.value;
-    // console.log("HERE:" + tempPriceInput);
     cell3.setAttribute("data-price", priceInput.value);
     cell3.setAttribute("data-toggle", "modal");
     cell3.setAttribute("data-target", "#exampleModal");
@@ -131,27 +115,14 @@ function saveBtnClick() {
 }
 
 function deleteBtnClick() {
-    // if (mainDiagram.childNodes[i].innerHTML == this.parentNode.childNodes[1].dataset) {
     let name;
     if (this.parentNode.childNodes[3])
         name = this.parentNode.childNodes[3].dataset.name;
     else 
         name = this.parentNode.childNodes[1].dataset.name;
 
-    console.log("asd");
-    console.log(this.parentNode.childNodes[1].dataset.name);
-    console.log("asd");
-    
-        // name = 
-    // console.log(this.parentNode.childNodes[1].dataset);
-    // console.log(this.parentNode.childNodes[3].innerHTML);
     for (let i = 1; i < mainDiagram.childNodes.length; i++) {
-        // console.log(mainDiagram.childNodes[i].innerHTML);
-        // console.log(this.parentNode.childNodes[3].dataset);
-        console.log(this.parentNode.childNodes[i]);
-
         if (mainDiagram.childNodes[i].innerHTML == name) {
-            console.log("deleted");
             mainDiagram.childNodes[i].remove();
             break;
         }
@@ -160,12 +131,6 @@ function deleteBtnClick() {
 }
 
 function changeBtnClick(input) {
-    // console.log("1: " + input);
-
-    console.log("asd");
-    console.log(input.parentNode.childNodes);
-    console.log("asd");
-
     let name;
     if (input.parentNode.childNodes[3])
         name = input.parentNode.childNodes[3].dataset.name;
@@ -177,8 +142,6 @@ function changeBtnClick(input) {
         value = input.parentNode.childNodes[5].dataset.price;
     else 
         value = input.parentNode.childNodes[2].dataset.price;
-
-    console.log(name, value);
 
     nameInput.value = tempNameInput = name;
     priceInput.value = tempPriceInput = value;
