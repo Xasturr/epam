b4e95f2ea1c3bf071d348e38ff27652e458fd689
Dmytro Leshchenko <dima.leshchenko20@gmail.com>
2020-05-28 12:39:42 +0300

    .

--- carona/client/src/config/config.js
+++ carona/client/src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    // url: "https://carona-server.herokuapp.com", 
-    // client: "https://carona-client.herokuapp.com",
-    url: "http://localhost:5000", 
-    client: "http://localhost:3000" ,
+    url: "https://carona-server.herokuapp.com", 
+    client: "https://carona-client.herokuapp.com",
+    // url: "http://localhost:5000", 
+    // client: "http://localhost:3000" ,
     apiKey: "AIzaSyA9y7yMC__s-qjfJAY1xnc5N2D15jvNjXo"
 }
